f256c45ab98041c580de2eadf384e959948397a1
Lizaveta <lizazaytseva16@gmail.com>
2021-09-16 18:28:29 +0300

    The main structure of the project

---
+++ TracerLib/ITracer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TracerLib
+{
+    interface ITracer
+    {
+        void StartTrace();​
+    
+        void StopTrace();​
+   
+        TraceResult GetTraceResult();
+    }
+}
---
+++ TracerLib/Tracer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TracerLib
+{
+    public class Tracer : ITracer
+    {
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult();
+        }
+
+        public void StartTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
